1a8d4fd545810f9d185ddde98011b29bc4bee1ec
Bhuvaneswar <bhuvaneswar.k@nichi.com>
2015-10-15 12:39:42 +0530

    Initial commit

---
+++ app/src/main/java/com/govarishi/expenser/com/govarishi/expenser/viewholder/IncomeCategoryViewHolder.java
@@ -0,0 +1,11 @@
+package com.govarishi.expenser.com.govarishi.expenser.viewholder;
+
+import android.support.v7.widget.RecyclerView;
+
+import static android.support.v7.widget.RecyclerView.ViewHolder;
+
+/**
+ * Created by nichiuser on 12-06-2015.
+ */
+public class IncomeCategoryViewHolder {
+}
